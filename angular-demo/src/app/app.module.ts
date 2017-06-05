06e49d15d8824bb5321d5f0c1f0b0a49cc9ed28d
RaviSankarRao <ravisankar139@gmail.com>
2017-06-05 14:52:48 -0700

    added shared folder and moved alerts into it

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -5,13 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { InfoAlertComponent } from './info-alert/info-alert.component';
-import { DangerAlertComponent } from './danger-alert/danger-alert.component';
+import { InfoAlertComponent } from './shared/info-alert/info-alert.component';
+import { DangerAlertComponent } from './shared/danger-alert/danger-alert.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { IfElseComponent } from './if-else/if-else.component';
 import { StyleDirectiveComponent } from './style-directive/style-directive.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './shared/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './shared/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
