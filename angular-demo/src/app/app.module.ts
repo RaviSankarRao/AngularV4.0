f0fd0f75d28a84c0031283d94dce1721d588aed6
RaviSankarRao <ravisankar139@gmail.com>
2017-06-05 14:54:36 -0700

    added concepts folder and moved componenets into it

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -7,9 +7,9 @@ import { ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { InfoAlertComponent } from './shared/info-alert/info-alert.component';
 import { DangerAlertComponent } from './shared/danger-alert/danger-alert.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { IfElseComponent } from './if-else/if-else.component';
-import { StyleDirectiveComponent } from './style-directive/style-directive.component';
+import { DataBindingComponent } from './concepts/data-binding/data-binding.component';
+import { IfElseComponent } from './concepts/if-else/if-else.component';
+import { StyleDirectiveComponent } from './concepts/style-directive/style-directive.component';
 import { WarningAlertComponent } from './shared/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './shared/success-alert/success-alert.component';
 
